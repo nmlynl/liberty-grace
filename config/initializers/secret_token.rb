a303139cab215996e795939ebce864f96bbb6daf
nml <nml@Nicholass-MacBook-Pro.local>
2013-08-26 12:55:35 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LibertyGrace::Application.config.secret_key_base = '6fe04119ec20eb072ef9af4ad59f99e4ecf8c0bd71ef196d452c412284506bedb1fb10058e1639f47c99dae3aad200adadb6ee78df9e886125144767fdf0527a'
